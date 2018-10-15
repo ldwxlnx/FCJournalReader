5d5409a6db55a4c44d442c7b9397c84321c82d29
KL <kludwig@hs-harz.de>
2018-10-15 09:34:50 +0200

    configuration obvoiulsy unstaged

--- src/de/ovgu/spldev/featurecopp/Configuration.java
+++ src/de/ovgu/spldev/featurecopp/Configuration.java
@@ -5,5 +5,14 @@ import java.io.File;
 public class Configuration {
 	public final static String LINESEP = System.lineSeparator();
 	public static long SHOW_TOP_N_FEATURES = Long.MAX_VALUE;
-	public static final File LOGFILE = new File("fcjournalreader.log");
+	public static File LOGFILE = new File("fcjournalreader.log");
+	public static File PROJCSV = new File("fcproj.txt");
+	public static File FEATCSV = new File("fcfeat.txt");
+	public static void setOutfiles(File outdir) {
+		if(outdir.isDirectory()) {
+			LOGFILE = new File(outdir, LOGFILE.toString());
+			PROJCSV = new File(outdir, PROJCSV.toString());
+			FEATCSV = new File(outdir, FEATCSV.toString());
+		} 
+	}
 }
